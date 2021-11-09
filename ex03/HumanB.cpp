a722fdd1ee5c8e7fa73d7115f8dab01a48b178e9
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-09 17:19:45 +0100

    ex03: contructor sets `HumanB::_weapon` to NULL

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 17:44:52 by lrocca            #+#    #+#             */
-/*   Updated: 2021/11/08 17:57:32 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/11/09 17:08:52 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name): _name(name) {}
+HumanB::HumanB(std::string name): _name(name), _weapon(NULL) {}
 
 HumanB::HumanB(std::string name, Weapon& weapon):
 	_name(name), _weapon(&weapon) {}
