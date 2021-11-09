4336785f5e05034f835aafa695b6d6d064625508
Leonardo Rocca <lrocca@student.42roma.it>
2021-11-09 17:18:09 +0100

    ex01: add name constructor

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 22:01:09 by lrocca            #+#    #+#             */
-/*   Updated: 2021/11/08 17:10:45 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/11/09 17:06:08 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 #include "Zombie.hpp"
 
 Zombie::Zombie(): _name("zombie") {}
+
+Zombie::Zombie(std::string name): _name(name) {}
 
 Zombie::~Zombie() {
 	std::cout << "<" << _name << "> Going back to my grave!" << std::endl;
--- ex01/Zombie.hpp
+++ ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: lrocca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 21:59:56 by lrocca            #+#    #+#             */
-/*   Updated: 2021/11/08 17:07:13 by lrocca           ###   ########.fr       */
+/*   Updated: 2021/11/09 17:06:17 by lrocca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ class Zombie {
 		std::string	_name;
 	public:
 		Zombie();
+		Zombie(std::string name);
 		~Zombie();
 		void	announce(void) const;
 		void	setName(std::string &name);
